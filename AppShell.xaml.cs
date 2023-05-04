15fbfce8bd47d9f2c01242b319b25315acaee232
Dalton Smith <105223895+daltzctr@users.noreply.github.com>
2023-05-04 15:52:26 -0400

    First commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SmallImageButtonRepro;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SmallImageButtonRepro;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
